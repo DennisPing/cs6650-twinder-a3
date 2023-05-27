d7928f3eb3faa82a9f6d826bd4a9a1b74bec83de
Dennis Ping <idennisping@gmail.com>
2023-05-26 23:07:31 -0400

    Changed MatchList data type from []int to map[string]struct{}

--- lib/models/dynamodb.go
+++ lib/models/dynamodb.go
@@ -3,8 +3,8 @@ package models
 // Using the aws-sdk-go-v2 library
 
 type DynamoUserStats struct {
-	UserId      int   `dynamodbav:"userId"`
-	NumLikes    int   `dynamodbav:"numLikes"`
-	NumDislikes int   `dynamodbav:"numDislikes"`
-	MatchList   []int `dynamodbav:"matchList"`
+	UserId      int                 `dynamodbav:"userId"`
+	NumLikes    int                 `dynamodbav:"numLikes"`
+	NumDislikes int                 `dynamodbav:"numDislikes"`
+	MatchList   map[string]struct{} `dynamodbav:"matchList"`
 }
