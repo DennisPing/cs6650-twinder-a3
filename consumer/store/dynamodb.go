645ffdfd0b2d26904399ef46780cc3c39d9d56af
Dennis Ping <idennisping@gmail.com>
2023-05-30 00:02:28 -0400

    Remove the NopRetryer

--- consumer/store/dynamodb.go
+++ consumer/store/dynamodb.go
@@ -29,9 +29,6 @@ func NewDatabaseClient() (*DatabaseClient, error) {
 	cfg, err := config.LoadDefaultConfig(
 		context.Background(),
 		config.WithRegion("us-east-2"),
-		config.WithRetryer(func() aws.Retryer {
-			return aws.NopRetryer{} // Don't retry errors
-		}),
 	)
 	if err != nil {
 		return nil, err
